13aac6fdbceb789833d22964d3a2b55d95b9f67c
Jens Bremer <jens.bremer@xs4all.nl>
2024-01-28 15:57:51 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Big code to answer the question "What is the maximum number of take-offs and landings (added up) that Schiphol handled within one hour in 2023?"
+
+
+Created on Sun Jan 28 15:55:41 2024
+
+@author: Max & Jens
+"""
+
+import numpy as np
+import pandas as pd
